0a01d8195c9bca75d86065039afd0bd5965bf60e
Umer Hassan <umerhassan@Umers-MacBook-Pro.local>
2022-09-10 01:26:32 -0400

    fetch and display content using web component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
+import MediaCard from 'ecm-web-components';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [media, setMedia] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/media')
+      .then(res => res.json())
+      .then(({ media }) => {
+        setMedia(media)
+      });
+  }, []);
+
   return (
     <div className="App">
+      <h1>Anime Media</h1>
+      <div className="media-list">
+        {media.map((media =>
+            <media-card title={media.title} image={media.image} desc={media.description}/>
+        ))}
+      </div>
 
     </div>
   );
